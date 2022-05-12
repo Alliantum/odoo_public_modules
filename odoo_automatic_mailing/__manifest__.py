f65a36e2d8367a97c541ff429630ef5f85a8f910
os-ia <os-ia@users.noreply.github.com>
2022-05-12 14:37:27 +0000

    Release odoo_automatic_mailing-v12.0.2.0.2

--- odoo_automatic_mailing/__manifest__.py
+++ odoo_automatic_mailing/__manifest__.py
@@ -6,7 +6,7 @@
     'website': "https://www.alliantum.com",
     'category': 'Technical Settings',
     'license': 'AGPL-3',
-    'version': '12.0.2.0.1',
+    'version': '12.0.2.0.2',
     'depends': [
         'sale',
         'account',
