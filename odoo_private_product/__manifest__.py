6fbd68cf11058ecb4978ff1c5a35e15289365285
os-ia <os-ia@users.noreply.github.com>
2022-05-12 14:56:53 +0000

    Release odoo_private_product-v12.0.1.0.1

--- odoo_private_product/__manifest__.py
+++ odoo_private_product/__manifest__.py
@@ -6,7 +6,7 @@
     """,
     'author': "Alliantum",
     'website': "https://www.alliantum.com",
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'license': 'AGPL-3',
     'category': 'Inventory',
     'depends': ['base', 'product', 'sale'],
