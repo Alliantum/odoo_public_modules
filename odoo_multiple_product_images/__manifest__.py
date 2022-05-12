37a97b6e8bfe7a50858de981da0c95e72c8f07f7
os-ia <os-ia@users.noreply.github.com>
2022-05-12 14:55:31 +0000

    Release odoo_multiple_product_images-v14.0.1.0.0

---
+++ odoo_multiple_product_images/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Multiple Images",
+    'summary': """
+        Multiple Images and Categories on Products""",
+    'description': """
+        Creates an image gallery section available on every product and also on the product templates.
+    """,
+    'author': "Alliantum",
+    'website': "https://www.alliantum.com/",
+    'category': 'Inventory',
+    'license': 'AGPL-3',
+    'version': '14.0.1.0.0',
+    'depends': ['product', 'stock', 'mrp'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/clean_orphans_images.xml',
+        'views/assets.xml',
+        'views/product_image_backend.xml',
+        
+    ]
+}
