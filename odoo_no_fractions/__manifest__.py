6424f6bec60da5654332c837927cfb7cd571315d
os-ia <os-ia@users.noreply.github.com>
2022-05-26 06:58:57 +0000

    Release odoo_no_fractions-v13.0.1.0.2

--- odoo_no_fractions/__manifest__.py
+++ odoo_no_fractions/__manifest__.py
@@ -5,7 +5,7 @@
     """,
     'author': "Alliantum",
     'website': "https://www.alliantum.com",
-    'version': '13.0.1.0.1',
+    'version': '13.0.1.0.2',
     'license': 'AGPL-3',
     'category': 'Warehouse',
     'depends': ['uom', 'sale', 'account'],
